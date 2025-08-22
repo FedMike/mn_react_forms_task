736d2d0fbf48af9f463298e182134dfda273e446
fedmike <molnek87@mail.ru>
2025-08-22 22:28:34 +0900

    updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  state = {
+    email: '',
+    isAgreeWithTerms: false,
+  }
+
+  handleChange = (e) => {
+    this.setState({[e.target.name]: e.target.value})
+  }
+
+  handleCheckboxChange = (e) => {
+    this.setState({[e.target.name]: e.target.checked})
+  }
+
+  handleSubmitForm = (e) => {
+    e.preventDefault();
+    if (this.validateEmail() && this.checkConsent()) {
+      if (this.state.isAgreeWithTerms) {
+        alert('The form has been successfully submitted')
+      }
+    }
+  }
+
+  validateEmail = () => {
+    if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(this.state.email)) {
+      alert('Uncorrect email');
+      return false
+    }
+    return true
+  }
+
+  checkConsent = () => {
+    if (!this.state.isAgreeWithTerms) {
+      alert('Your consent is required');
+      return false
+    }
+    return true
+  }
+  
+  render() {
+    const { email, isAgreeWithTerms } = this.state;
+
+        return (
+            <div>
+                <input
+                    type="email"
+                    name="email"
+                    placeholder="email"
+                    value={email}
+                    onChange={this.handleChange}
+                />
+                <br />
+                <label>
+                    <input
+                        type="checkbox"
+                        name="isAgreeWithTerms"
+                        checked={isAgreeWithTerms}
+                        onChange={this.handleCheckboxChange}
+                    />
+                    I agree with terms and conditions
+                </label>
+                <br />
+                <button onClick={this.handleSubmitForm}>Send</button>
+            </div>
+        );
+  }
 }
-
-export default App;
